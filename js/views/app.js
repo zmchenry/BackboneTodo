97568419d541dcabec13f9023340e10e9c087376
McHenry, Zachary <Zachary_McHenry@intuit.com>
2014-07-12 21:55:35 -0700

    Added tracking of completion and deletion of todos and routing

--- js/views/app.js
+++ js/views/app.js
@@ -29,7 +29,6 @@ app.AppView = Backbone.View.extend({
       this.listenTo(app.Todos, 'add', this.addOne);
       this.listenTo(app.Todos, 'reset', this.addAll);
 
-      // New
       this.listenTo(app.Todos, 'change:completed', this.filterOne);
       this.listenTo(app.Todos,'filter', this.filterAll);
       this.listenTo(app.Todos, 'all', this.render);
--- js/views/todos.js
+++ js/views/todos.js
@@ -6,7 +6,9 @@ app.TodoView = Backbone.View.extend({
     template: _.template($("#item-template").html()),
 
     events: {
+        "click .toggle": "togglecompleted",
         "dblclick label": 'edit',
+        "click .destroy": "clear",
         "keypress .edit": "updateOnEnter",
         "blur .edit": "close"
     },
@@ -14,13 +16,35 @@ app.TodoView = Backbone.View.extend({
     // Listen for changes to its model
     initialize: function() {
         this.listenTo(this.model, "change", this.render);
+        this.listenTo(this.model, "destroy", this.remove);
+        this.listenTo(this.model, "visible", this.toggleVisible);
     },
 
     // Re-render the title of the todo item.
     render: function() {
         this.$el.html(this.template(this.model.attributes));
+
+        this.$el.toggleClass("completed", this.model.get("completed"));
+        this.toggleVisible();
+
         this.$input = this.$(".edit");
         return this;
+    },
+
+    toggleVisible: function() {
+        this.$el.toggleClass("hidden", this.isHidden());
+    },
+
+    // See if items should be hidden
+    isHidden: function() {
+        var isCompleted = this.model.get("completed");
+        return (
+            (!isCompleted && app.TodoFilter === "completed")
+            || (isCompleted && app.TodoFilter === "active"));
+    },
+
+    togglecompleted: function() {
+      this.model.toggle();
     },
 
     // Switch to editing mode
@@ -34,6 +58,8 @@ app.TodoView = Backbone.View.extend({
 
         if(value) {
             this.model.save({title:value});
+        } else {
+            this.clear();
         }
 
         this.$el.removeClass("editing");
@@ -43,5 +69,9 @@ app.TodoView = Backbone.View.extend({
         if(e.which === ENTER_KEY) {
             this.close();
         }
+    },
+
+    clear: function() {
+        this.model.destroy();
     }
 });
